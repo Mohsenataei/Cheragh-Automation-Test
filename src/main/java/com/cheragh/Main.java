a5bdb42fdf25780e0b3b6915a7e5c3c4d2f30fac
mohsen <mohsenataeiasad@gmail.com>
2020-04-22 18:24:39 +0430

    just started testing

---
+++ src/main/java/com/cheragh/Main.java
@@ -0,0 +1,41 @@
+package com.cheragh;
+
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String BASE_URL = "https://admin:123qweasd@tmfdev.ir/";
+        String first_name = "mohsen";
+        String last_name = "ataei asad";
+        String mobile_number = "09330192943";
+        FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.manage().window().maximize();
+
+        webDriver.get(BASE_URL);
+        WebDriverWait wait = new WebDriverWait(webDriver,5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("register_first_name_input")));
+
+
+//        webDriver.findElement(By.xpath("//a[text()='ورود یا ثبت نام']")).click();
+        webDriver.findElement(By.id("register_first_name_input")).sendKeys(first_name);
+        webDriver.findElement(By.id("register_last_name_input")).sendKeys(last_name);
+        webDriver.findElement(By.id("register_mobile_input")).sendKeys(mobile_number);
+
+        webDriver.findElement(By.id("register_submit_button")).click();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+    }
+}
